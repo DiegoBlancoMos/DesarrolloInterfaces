788f4e3e1bd2b06a0b89caa2f43f8a0918198ec9
DiegoBlancoMos <dblancom23@fpcoruna.afundacion.org>
2024-10-14 17:22:42 +0200

    Ejercicio 4 Finalizado.

---
+++ sprint1tkinter/sprint1tkinter/Ejercicio4.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+
+# Función que actualiza la etiqueta según las aficiones seleccionadas
+def actualizar_aficiones():
+    aficiones_seleccionadas = []
+
+    if var_leer.get():
+        aficiones_seleccionadas.append("Leer")
+    if var_deporte.get():
+        aficiones_seleccionadas.append("Deporte")
+    if var_musica.get():
+        aficiones_seleccionadas.append("Música")
+
+    if aficiones_seleccionadas:
+        etiqueta.config(text="Aficiones seleccionadas: " + ", ".join(aficiones_seleccionadas))
+    else:
+        etiqueta.config(text="No has seleccionado ninguna afición.")
+
+root = tk.Tk()
+root.title("Ejercicio 4")
+root.geometry("300x200")
+
+var_leer = tk.BooleanVar()
+var_deporte = tk.BooleanVar()
+var_musica = tk.BooleanVar()
+
+etiqueta = tk.Label(root, text="")
+etiqueta.pack()
+
+check_leer=tk.Checkbutton(root,text="LEER",variable=var_leer,command=actualizar_aficiones)
+check_leer.pack()
+check_deporte=tk.Checkbutton(root,text="DEPORTE",variable=var_deporte,command=actualizar_aficiones)
+check_deporte.pack()
+check_musica=tk.Checkbutton(root,text="MÚSICA",variable=var_musica,command=actualizar_aficiones)
+check_musica.pack()
+
+
+
+
+
+
+
+root.mainloop()
