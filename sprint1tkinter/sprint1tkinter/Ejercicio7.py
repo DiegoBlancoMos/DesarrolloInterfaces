028f18af5e59eb3bb2e4603fd91a8bf579b85d42
DiegoBlancoMos <dblancom23@fpcoruna.afundacion.org>
2024-10-16 09:50:26 +0200

    Ejercicio 7 mejorado.

--- sprint1tkinter/sprint1tkinter/Ejercicio7.py
+++ sprint1tkinter/sprint1tkinter/Ejercicio7.py
@@ -1,27 +1,33 @@
 import tkinter as tk
 
-def dibujar_formas():
-    # Obtener las coordenadas y el tamaño del círculo y el rectángulo
+def dibujar_rectangulo():
+    # Obtener las coordenadas y el tamaño rectángulo
     try:
-        #INTRODUCIMOS LAS COORDENADAS DEL CIRCULO Y DEL RECTÁNGULO EN VARIABLES
-        x_circulo = int(entry_x_circulo.get())
-        y_circulo = int(entry_y_circulo.get())
-        radio = int(entry_radio.get())
+        #INTRODUCIMOS LAS COORDENADAS DEL RECTÁNGULO EN VARIABLES
 
         x_rectangulo = int(entry_x_rectangulo.get())
         y_rectangulo = int(entry_y_rectangulo.get())
         ancho = int(entry_ancho.get())
         alto = int(entry_alto.get())
 
-        # Dibujar el círculo
-        canvas.create_oval(x_circulo - radio, y_circulo - radio,
-                           x_circulo + radio, y_circulo + radio,
-                           outline="blue", fill="lightblue")
-
         # Dibujar el rectángulo
         canvas.create_rectangle(x_rectangulo, y_rectangulo,
                                 x_rectangulo + ancho, y_rectangulo + alto,
                                 outline="red", fill="lightcoral")
+    except ValueError:
+        print("Por favor, ingresa valores válidos.")
+def dibujar_circulo():
+    # Obtener las coordenadas y el tamaño del círculo
+    try:
+        #INTRODUCIMOS LAS COORDENADAS DEL CIRCULO EN VARIABLES
+        x_circulo = int(entry_x_circulo.get())
+        y_circulo = int(entry_y_circulo.get())
+        radio = int(entry_radio.get())
+
+        # Dibujar el círculo
+        canvas.create_oval(x_circulo - radio, y_circulo - radio,
+                           x_circulo + radio, y_circulo + radio,
+                           outline="blue", fill="lightblue")
     except ValueError:
         print("Por favor, ingresa valores válidos.")
 
@@ -43,6 +49,9 @@ entry_y_circulo.pack()
 entry_radio = tk.Entry(root)
 entry_radio.pack()
 
+boton_dibujar = tk.Button(root, text="Dibujar Circulo", command=dibujar_circulo)
+boton_dibujar.pack()
+
 # Campos de entrada para el rectángulo
 tk.Label(root, text="Rectángulo (x, y, ancho, alto):").pack()
 entry_x_rectangulo = tk.Entry(root)
@@ -55,7 +64,7 @@ entry_alto = tk.Entry(root)
 entry_alto.pack()
 
 # Botón para dibujar
-boton_dibujar = tk.Button(root, text="Dibujar", command=dibujar_formas)
+boton_dibujar = tk.Button(root, text="Dibujar Circulo", command=dibujar_rectangulo)
 boton_dibujar.pack()
 
 # Iniciar el bucle principal de la interfaz
