61ed108a3fb996738e242fbce3efb936ff05c99e
DiegoBlancoMos <dblancom23@fpcoruna.afundacion.org>
2024-11-20 10:44:28 +0100

    Ejercicio 3 Completado.

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,6 +1,11 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,21 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btnMeGusta = findViewById(R.id.btnMeGusta);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.yeremay);
+        btnMeGusta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Muestra un mensaje Toast al hacer clic en el botón
+                Toast.makeText(DetailActivity.this, "ME GUSTA", Toast.LENGTH_SHORT).show();
+
+                // Aquí puedes agregar la lógica para navegar a una nueva actividad (detalle)
+                // Por ejemplo, si tienes una actividad llamada DetailActivity:
+                Intent intent = new Intent(DetailActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
